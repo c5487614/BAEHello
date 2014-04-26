261deb3c9c584e67f47d9181d9fdc353cda13c4c
ChunhuiChen <ChunhuiChen>
2014-04-26 10:46:35 +0800

    test

--- src/cch/util/url/GitTest.java
+++ src/cch/util/url/GitTest.java
@@ -3,5 +3,9 @@ package cch.util.url;
 public class GitTest {
 	public GitTest(){
 		System.out.println("test");
+		
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 	}
 }
